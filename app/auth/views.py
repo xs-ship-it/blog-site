35d2ca3d8d018e8c66e19eb864bebd6801c33faa
Felix Nderitu <newtnfelix@gmail.com>
2021-11-17 08:46:47 +0300

    create a for registration function

--- app/auth/views.py
+++ app/auth/views.py
@@ -5,3 +5,17 @@ from .forms import RegistrationForm,LoginForm
 from ..import db
 from flask_login import login_user,logout_user,login_required
 from ..email import mail_message
+
+@auth.route('/register',methods = ["GET","POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data,password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        mail_message("Welcome to Blog app","email/welcome_user",user.email,user=user)
+
+        return redirect(url_for('auth.login'))
+        title = "New Account"
+    return render_template('auth/register.html',registration_form = form)
