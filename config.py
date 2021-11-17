fc41094116fc146a70d3a3a5d171c55cb33a0937
Felix Nderitu <newtnfelix@gmail.com>
2021-11-17 19:09:24 +0300

    update app

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-
 import os
+
 class Config:
     SECRET_KEY = 'felix'
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/blog90"
@@ -10,15 +10,19 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
+        
 class TestConfig(Config):
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/blog90"
+    
 class DevConfig(Config):
     SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/blog90"
     DEBUG = True
+    
 config_options = {
     "development": DevConfig,
     "production": ProdConfig,
