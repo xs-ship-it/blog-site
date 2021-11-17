643ac13605f73198fd716d288d59354a028d6abc
Felix Nderitu <newtnfelix@gmail.com>
2021-11-17 18:28:00 +0300

    change db user and password

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 class Config:
     SECRET_KEY = 'mwendaB'
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringaschool:mwendaB@localhost/xsblogs"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/xsblogs"
     UPLOADED_PHOTOS_DEST = "app/static/photos"
     # email configurations
     MAIL_SERVER = "smtp.gmail.com"
@@ -15,9 +15,9 @@ class ProdConfig(Config):
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringaschool:mwendaB@localhost/xsblogs"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/xsblogs"
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://moringaschool:mwendaB@localhost/xsblogs"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/xsblogs"
     DEBUG = True
 config_options = {
     "development": DevConfig,
