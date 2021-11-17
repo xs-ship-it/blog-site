1407d3e3e2c12e9ed23e338bc8f1c31bc2af8a72
Felix Nderitu <newtnfelix@gmail.com>
2021-11-17 18:36:47 +0300

    change database

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 
 import os
 class Config:
-    SECRET_KEY = 'mwendaB'
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/xsblogs"
+    SECRET_KEY = 'felix'
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/blog90"
     UPLOADED_PHOTOS_DEST = "app/static/photos"
     # email configurations
     MAIL_SERVER = "smtp.gmail.com"
@@ -15,60 +15,12 @@ class ProdConfig(Config):
     if SQLALCHEMY_DATABASE_URI and SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace("postgres://", "postgresql://", 1)
 class TestConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/xsblogs"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/blog90"
 class DevConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/xsblogs"
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://felix:324b21@localhost/blog90"
     DEBUG = True
 config_options = {
     "development": DevConfig,
     "production": ProdConfig,
     "test": TestConfig
 }
-# import os 
-
-# class Config:
-#     '''
-#     General configuration parent class
-#     '''
-#     QUOTE_API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
-#     SECRET_KEY = os.environ.get('SECRET_KEY')
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://felix:324b21@localhost/xsblogs'
-#     UPLOADED_PHOTOS_DEST = 'app/static/photos'
-
-#     #email configurations
-#     MAIL_SERVER = 'smtp.googlemail.com'
-#     MAIL_PORT = 587
-#     MAIL_USE_TLS = True
-#     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-#     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
-
-
-# class ProdConfig(Config):
-#     '''
-#     Production  configuration child class
-
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-#     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "")
-#     if SQLALCHEMY_DATABASE_URI.startswith("postgres://"):
-#         SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace(
-#             "postgres://", "postgresql://", 1)
-
-
-# class DevConfig(Config):
-#     '''
-#     Development  configuration child class
-
-#     Args:
-#         Config: The parent configuration class with General configuration settings
-#     '''
-
-#     DEBUG = True
-
-# config_options = {
-# 'development':DevConfig,
-# 'production':ProdConfig,
-# 'test': TestConfig,
-# }
